73bc4b6c0ca30d068e87e93f5935ea5fbaa5a688
Doaa Essam <doaa.essam@meng-tech.com>
2021-03-03 23:43:04 +0200

    adjust date in posts

--- src/app/@app/pages/components/home/home.component.ts
+++ src/app/@app/pages/components/home/home.component.ts
@@ -1,41 +1,32 @@
-import { DatePipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { PagesService } from 'app/@core/utils/service/pages.service';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { PagesService } from "app/@core/utils/service/pages.service";
 @Component({
-  selector: 'ngx-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  selector: "ngx-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router,private _pagesService:PagesService,private datePipe:DatePipe) {
-    this. getPosts();
+  constructor(private router: Router, private _pagesService: PagesService) {
+    this.getPosts();
   }
-  missedPosts:any;
-  foundPosts:any;
-  getPosts(){
-    this._pagesService
-    .getPosts('missed')
-    .subscribe((res) => {
-      this.missedPosts=res;
+  missedPosts: any;
+  foundPosts: any;
+  getPosts() {
+    this._pagesService.getPosts("missed").subscribe((res) => {
+      this.missedPosts = res;
       console.log(this.missedPosts);
     });
-    this._pagesService
-    .getPosts('found')
-    .subscribe((res) => {
-      this.foundPosts=res;
+    this._pagesService.getPosts("found").subscribe((res) => {
+      this.foundPosts = res;
       console.log(this.foundPosts);
     });
   }
   backgroundUrl() {
-    return 'assets/images/header-home.png';
+    return "assets/images/header-home.png";
   }
   addPost(e) {
-    this.router.navigate(['pages/add-post',e]);
-  }
-  adjustDate(dateString) {
-    const dateParsed = dateString.split('T')[0];
-    return this.datePipe.transform(dateParsed, 'MM-dd-yyyy');
+    this.router.navigate(["pages/add-post", e]);
   }
   ngOnInit() {}
 }
--- src/app/@shared/home-post-card/home-post-card.component.ts
+++ src/app/@shared/home-post-card/home-post-card.component.ts
@@ -1,32 +1,28 @@
-import { Component, Input, OnInit, TemplateRef } from '@angular/core';
-import { NbDialogService } from '@nebular/theme';
+import { DatePipe } from "@angular/common";
+import { Component, Input, OnInit, TemplateRef } from "@angular/core";
+import { NbDialogService } from "@nebular/theme";
 @Component({
-  selector: 'ngx-home-post-card',
-  templateUrl: './home-post-card.component.html',
-  styleUrls: ['./home-post-card.component.scss'],
+  selector: "ngx-home-post-card",
+  templateUrl: "./home-post-card.component.html",
+  styleUrls: ["./home-post-card.component.scss"],
 })
-export class HomePostCardComponent implements OnInit {
-  @Input() userName: string;
-  @Input() postedDate: string;
-  @Input() animalType: string;
-  @Input() location: string;
-  @Input() imgUrl: string;
-  @Input() description: string;
-  @Input() cardData:any;
-  constructor(private dialogService: NbDialogService) {
+export class HomePostCardComponent {
+  @Input() cardData: any;
+  constructor(
+    private dialogService: NbDialogService,
+    private datePipe: DatePipe
+  ) {}
+  adjustDate(dateString) {
+    const dateParsed = dateString.split("T")[0];
+    return this.datePipe.transform(dateParsed, "MM-dd-yyyy");
   }
-
-  ngOnInit(): void {
-    console.log(this.cardData);
-  }
-
   open(dialog: TemplateRef<any>) {
     this.dialogService.open(dialog, {
       closeOnEsc: true,
-      dialogClass: 'dialog',
+      dialogClass: "dialog",
     });
   }
   backgroundUrl() {
-    return 'assets/images/dog2.jpg';
+    return "assets/images/dog2.jpg";
   }
 }
